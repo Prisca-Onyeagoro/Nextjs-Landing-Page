3bee8e367c0651de7e4c0c5de606868dd83a88f6
prisca-onyeagoro <priscaonyeagoro@gmail.com>
2024-12-26 18:16:21 +0100

    tablet view hero page

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Navbar from "@/sections/Navbar";
+import Hero from "@/sections/Hero";
 export default function Home() {
     return (
         <div>
             <Navbar />
+            <Hero />
         </div>
     );
 }
