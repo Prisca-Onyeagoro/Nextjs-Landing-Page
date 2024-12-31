7340f41fa660c37e6f670a6507968d22b8d2a5cd
prisca-onyeagoro <priscaonyeagoro@gmail.com>
2024-12-31 15:02:37 +0100

    tag section

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
 import LogoTicker from "@/sections/LogoTicker";
+import Introduction from "@/sections/Introduction";
 export default function Home() {
     return (
         <div>
             <Navbar />
             <Hero />
             <LogoTicker />
+            <Introduction />
         </div>
     );
 }
--- landing-page/src/sections/Introduction.tsx
+++ landing-page/src/sections/Introduction.tsx
@@ -1,5 +1,16 @@
 const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
 
 export default function Introduction() {
-    return <div>Introduction</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div>Introducing Layers</div>
+                <div>
+                    <span>Your creative process deserves better.</span>
+                    <span>{text}</span>
+                    <span>That&apos;s why we built Layers</span>
+                </div>
+            </div>
+        </section>
+    );
 }
