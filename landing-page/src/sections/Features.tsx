1700b334876cec718388c20e7e7e8ec8758f09c3
prisca-onyeagoro <priscaonyeagoro@gmail.com>
2025-01-04 20:08:26 +0100

    featurecard

--- landing-page/src/sections/Features.tsx
+++ landing-page/src/sections/Features.tsx
@@ -1,3 +1,12 @@
+import FeatureCard from "@/components/FeatureCard";
+import Tag from "@/components/Tag";
+import ava1 from "@/assets/images/avatar-ashwin-santiago.jpg";
+import ava2 from "@/assets/images/avatar-florence-shaw.jpg";
+import ava3 from "@/assets/images/avatar-lula-meyers.jpg";
+import ava4 from "@/assets/images/avatar-owen-garcia.jpg";
+import Image from "next/image";
+import Avatar from "@/components/Avatar";
+
 const features = [
     "Asset Library",
     "Code Preview",
@@ -9,5 +18,76 @@ const features = [
 ];
 
 export default function Features() {
-    return <div>Features</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                    <Tag>Features</Tag>
+                </div>
+                <h2 className="text-6xl font-medium text-center mt-6 ">
+                    Where power meets{" "}
+                    <span className="text-lime-400">Simplicity</span>
+                </h2>
+                <div className="mt-12 grid grid-cols-1 gap-8">
+                    <FeatureCard
+                        title="Real-time Collaboration"
+                        description="  Work together seamlessly with conflict-free team
+                                editing"
+                    >
+                        <div className="aspect-video flex items-center justify-center">
+                            <Avatar className="z-40">
+                                <Image
+                                    src={ava1}
+                                    alt="avatar1"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-indigo-500 z-30">
+                                <Image
+                                    src={ava2}
+                                    alt="avatar2"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-amber-500 z-20">
+                                <Image
+                                    src={ava3}
+                                    alt="avatar3"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border border-transparent ">
+                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1">
+                                    {Array.from({ length: 3 }).map((_, i) => (
+                                        <span
+                                            className="size-1.5 rounded-full bg-white inline-flex"
+                                            key={i}
+                                        ></span>
+                                    ))}
+                                </div>
+                            </Avatar>
+                        </div>
+                    </FeatureCard>
+                    <FeatureCard
+                        title="Interactive Prototyping"
+                        description=" Engage your clients that work with prototypes
+                                that reacts to user actions"
+                    ></FeatureCard>
+                    <FeatureCard
+                        title="Keyboard Quick Actions"
+                        description="Powerful commands to help ypu create designs
+                                more quickly"
+                    ></FeatureCard>
+                </div>
+                <div>
+                    {features.map((feature) => (
+                        <div key={feature}>
+                            <span></span>
+                            <span>{feature}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
